6429b174bc682ebd9734dc77735732fbdb1bbd85
Shanavastest <shantestingeng@gmail.com>
2026-01-31 10:37:14 +0530

    updatedtest

---
+++ test1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+a = 10
+b = 20
+c = 35
+d =45
+e =56
+x=4
+result = a + b +c + d +e +x
+# Streamlit app
+st.title("Addition App")
+st.text("Welcome")
+st.write(f"The sum of {a} and {b} is: {result}")
